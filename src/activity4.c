f3296adf1ea01072d7f2a624bc4528dbda627f67
sammy-9930 <80445152+sammy-9930@users.noreply.github.com>
2021-04-30 18:27:10 +0530

    Update activity4.c

--- src/activity4.c
+++ src/activity4.c
@@ -2,10 +2,10 @@
 
 void initUSART(uint16_t ubrr_value)
 {
-    SetBaudLow;
-    SetBaudHigh;
-    SetCharBits;
-    EnableReceiverAndTransmitter;
+    UBRR0L=ubrr_value;
+    UBRR0H=(ubrr_value>>8)&0x00ff;
+    UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
+    UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 }
 
 
