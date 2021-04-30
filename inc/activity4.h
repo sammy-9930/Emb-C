9d43f44cee36e6039801e12259d7cd422befa3d7
sammy-9930 <80445152+sammy-9930@users.noreply.github.com>
2021-04-30 18:25:44 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -18,12 +18,7 @@
  * @note delay header file
  */
 #include <util/delay.h>
-#define SetBaudLow (UBRR0L=ubrr_value)
-#define SetBaudHigh (UBRR0H=(ubrr_value>>8)&0x00ff)
-#define SetCharBits (UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00))
-#define EnableReceiverAndTransmitter (UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0))
-#define ReadOrWriteData (UDR0)
-#define WaitForTransmissionReady (!(UCSR0A&(1<<UDRE0)))
+
 /**
  * @brief initialize the USART registers
  * 
