9ec6b7cc395e588fc5fbd69cbeec452325a84327
sammy-9930 <80445152+sammy-9930@users.noreply.github.com>
2021-04-30 19:24:00 +0530

    Update activity2.c

--- src/activity2.c
+++ src/activity2.c
@@ -2,20 +2,20 @@
 
 void initADC()
 {
-   MuxReg=(1<<RefToVcc); // Aref=Avcc, set for 1024
-   CntrlReg=(1<<EnableADC)|(7<<SetFCPU); // Enable ADC and Set for FCPU 128
+    ADMUX = (1<<REFS0);
+    ADCSRA = (1<<ADEN)|(7<<ADPS0);
 }
 
 uint16_t ReadADC(uint8_t select_channel)
 {
-   MuxReg&=0xf8;
-   select_channel=select_channel&0b00000111;
-   MuxReg|=select_channel;
-   //Start single conversion
-   CntrlReg|=(1<<StrtConv);
-   //wait for conversion to complete
-   while(!(CntrlReg&(1<<ADCInterruptFlag)));
-   // clear ADIF by passing 1
-   CntrlReg|=(1<<ADCInterruptFlag);
-   return AtoDvalue;
+    ADMUX &= 0xf8;
+    ch = ch&0b00000111;
+    ADMUX |= ch;
+    //start conversion
+    ADCSRA |= (1<<ADSC);
+    //wait for conversion to complete
+    while(!(ADCSRA&(1<<ADIF)));
+    //clear ADIF by writing 1
+    ADCSRA |= (1<<ADIF);
+    return(ADC);
 }
