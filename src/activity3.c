f92d12c30bf5a06fa54294be9a5f594b0db168f0
sammy-9930 <80445152+sammy-9930@users.noreply.github.com>
2021-04-30 18:59:10 +0530

    Update activity3.c

--- src/activity3.c
+++ src/activity3.c
@@ -1,6 +1,6 @@
 #include "activity3.h"
 #define DUTY_CYCLE(Percent) (1024*Percent/100)
-void initPWM()
+void initializePWM();
 {
     TCCR1A=(1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
     TCCR1B=(1<<CS11)|(1<<WGM12)|(1<<CS10);
