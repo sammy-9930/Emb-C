3cda5396a52275bd329810b68eb6bdb678d71cf7
sammy-9930 <80445152+sammy-9930@users.noreply.github.com>
2021-04-30 16:22:39 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -1,1 +1,19 @@
-
+#include "activity1.h"
+void initialize_pins()
+{
+    DDRB |= (1<<PB0);//set as output
+    DDRD &= ~(1<<PD0);//set as input 
+    DDRD &= ~(1<<PD1);//set as input 
+    PORTD |= (1<<PD0);//set bit
+    PORTD |= (1<<PD1);//set bit
+}
+void turn_on_led()
+{
+  //turn on the LED
+   PORTB |= (1<<PB0);
+}
+void turn_off_led()
+{
+  //turn off the LED
+   PORTB |= ~(1<<PB0);
+}
