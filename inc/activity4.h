bbb2e06a24169cd172fa27a9da53b4917a77aa55
sammy-9930 <80445152+sammy-9930@users.noreply.github.com>
2021-04-30 16:40:52 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -1,6 +1,22 @@
+/**
+ * @file activity4.h
+ * @author Samantha Menezes
+ * @brief Read input from Adc and display on oscilloscope
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef ACTIVITY4_H_INCLUDED
 #define ACTIVITY4_H_INCLUDED
+/**
+ *@note avr header file 
+ */
 #include <avr/io.h>
+/**
+ * @note delay header file
+ */
 #include <util/delay.h>
 #define SetBaudLow (UBRR0L=ubrr_value)
 #define SetBaudHigh (UBRR0H=(ubrr_value>>8)&0x00ff)
@@ -8,7 +24,17 @@
 #define EnableReceiverAndTransmitter (UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0))
 #define ReadOrWriteData (UDR0)
 #define WaitForTransmissionReady (!(UCSR0A&(1<<UDRE0)))
+/**
+ * @brief initialize the USART registers
+ * 
+ * @param ubrr_value Baud rate
+ */
 void initUSART(uint16_t ubrr_value);
+/**
+ * @brief To write data through serial transmission
+ * 
+ * @param data input data to be transmitted
+ */
 void WriteCharUSART(char data);
 
 
