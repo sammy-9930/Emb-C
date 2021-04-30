f570d1d32d2c9c7ba95f719879b5abae23febcca
sammy-9930 <80445152+sammy-9930@users.noreply.github.com>
2021-04-30 16:30:04 +0530

    Update activity1.h

--- inc/activity1.h
+++ inc/activity1.h
@@ -1,12 +1,36 @@
+/**
+ * @file activity1.h
+ * @author Samantha Menezes
+ * @brief Led will turn on if both switches are on
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef ACTIVITY1_H_INCLUDED
 #define ACTIVITY1_H_INCLUDED
+/**
+ *@note avr header file 
+ */
 #include<avr/io.h>
 #define INPUTPIN (PIND)
 #define HEATERSWITCH (PD0)
 #define SEATED (PD1)
-
+/**
+ * @brief Initializing the pins to input and output  
+ * 
+ */
 void initialize_pins();
+/**
+ * @brief Logic to turn off Led 
+ * 
+ */
 void turn_off_led();
+/**
+ * @brief Logic to turn on Led 
+ * 
+ */
 void turn_on_led();
 
 #endif // ACTIVITY1_H_INCLUDED
